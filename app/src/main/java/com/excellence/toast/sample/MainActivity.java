82936c82f7d7c154f14a07d0cdf03e2a37e0afaf
master <690393541@qq.com>
2017-12-27 20:17:18 +0800

    Initial commit

---
+++ app/src/main/java/com/excellence/toast/sample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.excellence.toast.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.excellence.toast.SystemToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HintToast.getInstance(MainActivity.this).showMsg("hello");
+                // SystemToast.makeText(MainActivity.this, "Toast", SystemToast.TOAST_SHORT).show();
+            }
+        });
+    }
+}
